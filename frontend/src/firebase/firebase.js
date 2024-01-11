c6ddbf2392735511833d4811429dc2568cb4e381
Mayank13579 <mayankupadhyay13579@gmail.com>
2024-01-11 18:00:12 +0530

    restored

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -5,16 +5,19 @@ import "firebase/compat/firestore";
 
 const firebaseConfig = {
 
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-  
+  apiKey: "AIzaSyBXgx9ihV7BeLFwEhleH0gThSRYD9crkBc",
+    authDomain: "ecommerce-react-a1ea5.firebaseapp.com",
+    projectId: "ecommerce-react-a1ea5",
+    storageBucket: "ecommerce-react-a1ea5.appspot.com",
+    messagingSenderId: "652901800165",
+    appId: "1:652901800165:web:a97f3aa30b6c7e11619c0c",
+    measurementId: "G-5YQFMCCZQK"
+  // ...
 };
+// console.log('Firebase Configuration:', firebaseConfig);
+// ... other logging statements
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 
